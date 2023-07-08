61b0312f3df4dcfed35b75c61730075c7e882531
Вадим Антипов <vadim.v.antipov@gmail.com>
2023-07-08 15:33:46 +0300

    feat: маршрутизация

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,30 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { AppRoute } from '../../const';
 import MainScreen from '../../pages/main-screen/main-screen';
+import LoginScreen from '../../pages/login-screen/login-screen';
+import OfferScreen from '../../pages/offer-screen/offer-screen';
+import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
 
 type AppProps = {
   offersCount: number;
 };
 
 function App({ offersCount }: AppProps): JSX.Element {
-  return <MainScreen offersCount={offersCount} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainScreen offersCount={offersCount} />}
+        />
+        <Route path={AppRoute.Login} element={<LoginScreen />} />
+        <Route path={AppRoute.Offer} element={<OfferScreen />} />
+        <Route path={AppRoute.Favorites} element={<FavoritesScreen />} />
+        <Route path="*" element={<NotFoundScreen />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
