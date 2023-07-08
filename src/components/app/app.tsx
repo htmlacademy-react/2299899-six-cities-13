cf91b86316e20073adc04dee00938d120d4281c8
Вадим Антипов <vadim.v.antipov@gmail.com>
2023-07-08 15:40:20 +0300

    feat: приватный компонент для страницы favorites

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,10 +1,11 @@
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
-import { AppRoute } from '../../const';
+import { AppRoute, AuthorizationStatus } from '../../const';
 import MainScreen from '../../pages/main-screen/main-screen';
 import LoginScreen from '../../pages/login-screen/login-screen';
 import OfferScreen from '../../pages/offer-screen/offer-screen';
 import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
 import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
+import PrivateRoute from '../private-route/private-route';
 
 type AppProps = {
   offersCount: number;
@@ -20,7 +21,14 @@ function App({ offersCount }: AppProps): JSX.Element {
         />
         <Route path={AppRoute.Login} element={<LoginScreen />} />
         <Route path={AppRoute.Offer} element={<OfferScreen />} />
-        <Route path={AppRoute.Favorites} element={<FavoritesScreen />} />
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+              <FavoritesScreen />
+            </PrivateRoute>
+          }
+        />
         <Route path="*" element={<NotFoundScreen />} />
       </Routes>
     </BrowserRouter>
