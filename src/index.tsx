6186d64ee55cf399e637ebc04eacaca1c2630868
Вадим Антипов <vadim.v.antipov@gmail.com>
2023-07-08 12:20:05 +0300

    feat: передача пропсов в компонент главной страницы

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const MOCK_OFFERS_COUNT = 313;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App offersCount={MOCK_OFFERS_COUNT} />
   </React.StrictMode>
 );
