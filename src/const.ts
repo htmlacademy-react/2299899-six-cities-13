584ef961c86b2baf78aeab6ed4b9347fdccbe47d
Вадим Антипов <vadim.v.antipov@gmail.com>
2023-07-14 18:49:21 +0300

    fix: правка иконок карты

--- src/const.ts
+++ src/const.ts
@@ -21,7 +21,7 @@ export const CITIES = [
 ];
 
 export const URL_MARKER_DEFAULT =
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
+  'img/pin.svg';
 
 export const URL_MARKER_CURRENT =
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
+  'img/pin-active.svg';
