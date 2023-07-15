ee055c90b3abcaba0d138603282f4ff61ead5d55
Вадим Антипов <vadim.v.antipov@gmail.com>
2023-07-15 19:39:21 +0300

    fix: исправление сортировки

--- src/pages/main-page/sort-options.ts
+++ src/pages/main-page/sort-options.ts
@@ -1,10 +1,10 @@
-import { Offer } from '../../mocks/offer';
+import { Offer } from '../../types/offer';
 
 export const sortPriceLowToHigh = (offerA: Offer, offerB: Offer) =>
-  offerB.price - offerA.price;
-
-export const sortPriceHighToLow = (offerA: Offer, offerB: Offer) =>
   offerA.price - offerB.price;
 
+export const sortPriceHighToLow = (offerA: Offer, offerB: Offer) =>
+  offerB.price - offerA.price;
+
 export const sortTop = (offerA: Offer, offerB: Offer) =>
-  offerB.rate - offerA.rate;
+  offerB.rating - offerA.rating;
